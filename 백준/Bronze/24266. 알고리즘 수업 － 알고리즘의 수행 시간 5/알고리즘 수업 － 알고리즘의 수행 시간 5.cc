35fc20bf16ec62f16eee6669302577dc7f8b79cf
k1mbeaver <83329956+k1mbeaver@users.noreply.github.com>
2024-01-24 20:39:58 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long nCount;
+
+    cin >> nCount;
+    nCount = nCount * nCount * nCount;
+
+    cout << nCount << endl;
+    cout << 3 << endl;
+
+    return 0;
+}
