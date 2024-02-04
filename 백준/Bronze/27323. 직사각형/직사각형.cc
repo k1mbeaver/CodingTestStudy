d1b5795c9baf95775f8e40d3c25115d8c8fe86a3
k1mbeaver <83329956+k1mbeaver@users.noreply.github.com>
2024-02-04 23:27:56 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a, b;
+    int result;
+
+    cin >> a >> b;
+
+    result = a * b;
+
+    cout << result << endl;
+
+    return 0;
+}
