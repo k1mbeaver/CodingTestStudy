130cbca181450f4f14a4b9b9c8a3a82ea6215979
k1mbeaver <83329956+k1mbeaver@users.noreply.github.com>
2024-03-24 23:55:47 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string getStr;
+    int N;
+
+    cin >> getStr;
+    cin >> N;
+
+    cout << getStr[N - 1];
+
+    return 0;
+}
